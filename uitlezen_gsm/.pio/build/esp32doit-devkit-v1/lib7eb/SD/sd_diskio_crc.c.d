7fe450d79857b59eb744f327a3870841c98efd49
matsmortier <mats.mortier@gmail.com>
2023-02-28 13:15:57 +0100

    upd

---
+++ uitlezen_gsm/.pio/build/esp32doit-devkit-v1/lib7eb/SD/sd_diskio_crc.c.d
@@ -0,0 +1,2 @@
+.pio/build/esp32doit-devkit-v1/lib7eb/SD/sd_diskio_crc.c.o: \
+ C:/Users/Mats\ Mortier/.platformio/packages/framework-arduinoespressif32/libraries/SD/src/sd_diskio_crc.c
---
+++ uitlezen_gsm/.pio/build/esp32doit-devkit-v1/lib83f/GFX Library for Arduino/databus/Arduino_ESP8266SPI.cpp.d
@@ -0,0 +1,2 @@
+.pio/build/esp32doit-devkit-v1/lib83f/GFX\ Library\ for\ Arduino/databus/Arduino_ESP8266SPI.cpp.o: \
+ .pio/libdeps/esp32doit-devkit-v1/GFX\ Library\ for\ Arduino/src/databus/Arduino_ESP8266SPI.cpp
