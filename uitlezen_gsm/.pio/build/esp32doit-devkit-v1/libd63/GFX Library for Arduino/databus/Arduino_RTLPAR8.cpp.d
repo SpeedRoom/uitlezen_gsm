c89561cf8eb047fe38db36a3bb1f17c9d52c4ee8
SienPeerlinck <sienpeerlinck@hotmail.com>
2023-03-28 13:35:21 +0200

    OTA updated

---
+++ uitlezen_gsm/.pio/build/esp32doit-devkit-v1/libd63/GFX Library for Arduino/databus/Arduino_RTLPAR8.cpp.d
@@ -0,0 +1,2 @@
+.pio/build/esp32doit-devkit-v1/libd63/GFX\ Library\ for\ Arduino/databus/Arduino_RTLPAR8.cpp.o: \
+ .pio/libdeps/esp32doit-devkit-v1/GFX\ Library\ for\ Arduino/src/databus/Arduino_RTLPAR8.cpp
